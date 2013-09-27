cba6336e0ce950263ccb0687ee4b7486c49b8f82
FastFox <info@fastfox.nl>
2013-09-27 10:21:45 +0200

    Finish 3C

--- task3.m
+++ task3.m
@@ -1,37 +1,44 @@
 row = 0;
-figure
+figure(1)
+%figure(1, 'visible', 'off');
+
+rmseC = zeros(3, 9);
+
 for n = [9, 15, 100]
 	y = @(x) 0.5 + 0.4*sin(2*pi*x);
 
-	%px = rand(n, 1);
-	px = [1/n:1/n:1]';
-	py = rand(n, 1);
+	px = sort(rand(n, 1));
+	%px = [1/n:1/n:1]';
+	py = sort(rand(n, 1));
 
 	train = y(px) + 0.05*randn(n, 1);
 	test = y(py) + 0.05*randn(n, 1);
 
-	p = arrayfun(y, [1/n:1/n:1]);
+	p = arrayfun(y, [1/100:1/100:1]);
+
 	subplot(3, 2, row * 2 + 2);
 	hold on
-	plot([1/n:1/n:1], p, 'g')
-	plot([1/n:1/n:1], train, 'db')
-	%print -dpng '3b'
-
+	plot([1/100:1/100:1], p, 'g')
+	plot(px, train, 'db')
 
 	dBest = 1;
 	rmseBest = 1;
 	yBest = px;
 	rmse = zeros(9, 1);
 	for d = 1:9
-		q = polyfit(px, train, d);
-		y = polyval(q, px);
-		rmse(d) = sqrt(mean((train - y).^2));
-		%rmse = sqrt(mean((train - y).^2));
-		if rmse(d) < rmseBest
-			dBest = d;
-			rmseBest = rmse(d);
-			yBest = y;
-		end
+    if d < n
+		  q = polyfit(px, train, d);
+		  y = polyval(q, px);
+		  rmse(d) = sqrt(mean((train - y).^2));
+  
+		  rmseC(row + 1, d) = sqrt(mean((test - polyval(q, py)).^2));
+
+		  if rmse(d) < rmseBest
+			  dBest = d;
+			  rmseBest = rmse(d);
+			  yBest = y;
+		  end
+    end
 	end
 	plot(px, yBest, 'r');
 	title(['Best degree = ' num2str(dBest)]);
@@ -45,5 +52,23 @@ for n = [9, 15, 100]
 	ylabel('RMSE');
 	%dBest
 	%plot(px, yBest, 'r')
-	row = row + 1; 
+	%rmseC=	sqrt(mean((train - test).^2))
+  
+  %row
+  %size(py)
+  %rmseC
+  row = row + 1;
 end
+hold off
+print -dpng '3b'
+
+figure(2)
+n = [9, 15, 100];
+for i = [1, 2, 3]
+  subplot(1, 3, i);
+  plot([1:9], rmseC(i, :));
+  title(['RSME of train set to test set. N = ' num2str(n(i))]);
+  xlabel('Degree');
+  ylabel('RSME');
+end
+print -dpng '3c'
